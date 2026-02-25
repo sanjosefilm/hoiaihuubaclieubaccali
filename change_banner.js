e5d6c46e701db5d8caceb5f64d4ebc51c60609a6
sanjosefilm <sanjosefilm@gmail.com>
2026-02-24 21:53:12 -0800

    Add files via upload
    Signed-off-by: sanjosefilm <sanjosefilm@gmail.com>

--- change_banner.js
+++ change_banner.js
@@ -22,17 +22,25 @@ function imageRotator(){
 */	
 	
 function imageRotator() {
-var curImg = $('#imageShow li.current');
-var nextImg = curImg.next();
+	var curImg = $('#imageShow li.current');
+	var nextImg = curImg.next('li');
 
-if (nextImg.length == 0) {
-nextImg = $('#imageShow li:first');
-};
-
-curImg.removeClass('current').addClass('previous');
-nextImg.css({opacity:0}).addClass('current').animate({opacity:1},2000, function() {
-curImg.removeClass('previous');
-});
-
-
+	if (nextImg.length == 0) {
+		nextImg = $('#imageShow li:first');
+	}
+	
+	// Ensure current image is visible
+	curImg.css('opacity', 1);
+	
+	// Set next image to invisible and make it current
+	nextImg.css('opacity', 0).addClass('current');
+	
+	// Remove current class from previous and add previous class
+	curImg.removeClass('current').addClass('previous');
+	
+	// Animate the transition
+	nextImg.animate({opacity: 1}, 2000, function() {
+		// Clean up - remove previous class from old image
+		$('#imageShow li.previous').removeClass('previous').css('opacity', 0);
+	});
 };
